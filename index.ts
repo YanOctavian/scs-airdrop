4d6efed6ebdb8affd7f9614b070a9322bfaf1c85
YanOctavian <iu4ryhiuenb@proton.me>
2024-11-29 18:23:46 +0800

    完成空投脚本编写

--- index.ts
+++ index.ts
@@ -12,11 +12,7 @@ async function main () {
     console.log("Hello via Bun!");
     const airdrop = new Airdrop(privateKey, url);
     const results = await read_csv("addresses.csv");
-    console.log("hhhh: ", results[0]);
-    console.log("结束")
-    // const result = await airdrop.send(null, results[0].to, results[0].value);
     await airdrop.run(results);
-    // console.log("result: ", result);
 
 }
 
